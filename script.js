86b0a853f88695849bd9207b29ad4d5df2390489
zecorr <zecorr88@gmail.com>
2023-10-22 18:45:54 -0300

    Add round logic

--- script.js
+++ script.js
@@ -1,11 +1,64 @@
+// Global Variables
+const rock = "Rock"
+const paper = "Paper"
+const scissors = "Scissors"
+const choices = [rock, paper, scissors]
+const playerWins = "Player 1 wins!"
+const computerWins = "Computer Wins!"
+const draw = "Draw!"
+const randomPlayerChoice = Math.floor(Math.random() * 3)
+const randomComputerChoice = Math.floor(Math.random() * 3)
 
-// Randomly return either Rock, Paper, or Scissors for computer player.
-const getComputerChoice = function (){
-    const choices = ["Rock!", "Paper!", "Scissors!"]
-    const randomChoice = Math.floor(Math.random() * 3)
-    console.log(choices[randomChoice])
+// Randomly return either Rock, Paper, or Scissors.
+const getPlayerChoice = () => choices[randomPlayerChoice]
+const getComputerChoice = () => choices[randomComputerChoice]
 
+const player = getPlayerChoice()
+const computer = getComputerChoice()
+
+// Handle round logic
+const playRound = function (playerSelection, computerSelection){
+
+    if (playerSelection === computerSelection){
+        console.log(`${draw} Both players had ${computerSelection}`)
+        return draw
+    } else if (playerSelection === rock && computerSelection === paper){
+        console.log(`
+        Player 1 has: ${playerSelection} 
+        Computer has: ${computerSelection}`)
+        console.log(`${paper} beats ${rock}. ${computerWins}`)
+        return computerWins
+    } else if (playerSelection === rock && computerSelection === scissors){
+        console.log(`
+        Player 1 has: ${playerSelection} 
+        Computer has: ${computerSelection}`)
+        console.log(`${rock} beats ${scissors}! ${playerWins}`)
+        return playerWins
+    } else if (playerSelection === paper && computerSelection === rock){
+        console.log(`
+        Player 1 has: ${playerSelection} 
+        Computer has: ${computerSelection}`)
+        console.log(`${paper} beats ${rock}. ${playerWins}`)
+        return playerWins
+    } else if (playerSelection === paper && computerSelection === scissors){
+        console.log(`
+        Player 1 has: ${playerSelection} 
+        Computer has: ${computerSelection}`)
+        console.log(`${scissors} beats ${paper}! ${computerWins}`)
+        return computerWins
+    } else if (playerSelection === scissors && computerSelection === rock){
+        console.log(`
+        Player 1 has: ${playerSelection} 
+        Computer has: ${computerSelection}`)
+        console.log(`${rock} beats ${scissors}! ${computerWins}`)
+        return computerWins
+    } else if (playerSelection === scissors && computerSelection === paper){
+        console.log(`
+        Player 1 has: ${playerSelection} 
+        Computer has: ${computerSelection}`)
+        console.log(`${scissors} beats ${paper}! ${playerWins}`)
+        return playerWins
+    } 
 }
 
-getComputerChoice()
-
+playRound(player, computer) 
