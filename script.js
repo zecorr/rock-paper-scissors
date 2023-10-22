bfc256219f6258b4ed9476a3bdd94ad21b854ff1
zecorr <zecorr88@gmail.com>
2023-10-22 17:15:48 -0300

    Add function to handle computer choices

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hey bro!")
+
+// Randomly return either Rock, Paper, or Scissors for computer player.
+const getComputerChoice = function (){
+    const choices = ["Rock!", "Paper!", "Scissors!"]
+    const randomChoice = Math.floor(Math.random() * 3)
+    console.log(choices[randomChoice])
+
+}
+
+getComputerChoice()
+
